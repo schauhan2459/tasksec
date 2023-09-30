d165b16516eee1c9bb7dc07fa617905a2172d027
schauhan2459 <130307427+schauhan2459@users.noreply.github.com>
2023-09-30 13:29:26 +0530

    Update index.js
    chat server

--- public/chatter/nodeServer/index.js
+++ public/chatter/nodeServer/index.js
@@ -1,28 +1,28 @@
 // Node Server for socket io connections
 
 
-const io = require('socket.io')(3000, {
-    cors: {
-      origin: '*',
-    }
-  });
-const users = {};
+// const io = require('socket.io')(3000, {
+//     cors: {
+//       origin: '*',
+//     }
+//   });
+// const users = {};
 
 
-io.on('connection', socket => {
-    socket.on('new-user-joined', name => {
-        console.log("New user", name);
-        users[socket.id] = name;
-        socket.broadcast.emit('user-joined', name);
-    });
+// io.on('connection', socket => {
+//     socket.on('new-user-joined', name => {
+//         console.log("New user", name);
+//         users[socket.id] = name;
+//         socket.broadcast.emit('user-joined', name);
+//     });
 
-    socket.on('send', message => {
-        socket.broadcast.emit('receive', { message: message, name: users[socket.id] })
-    });
+//     socket.on('send', message => {
+//         socket.broadcast.emit('receive', { message: message, name: users[socket.id] })
+//     });
 
-    socket.on('disconnect', users => {
-        socket.broadcast.emit('left', users[socket.id]);
-        delete users[socket.id];
-    });
-})
+//     socket.on('disconnect', users => {
+//         socket.broadcast.emit('left', users[socket.id]);
+//         delete users[socket.id];
+//     });
+// })
 
