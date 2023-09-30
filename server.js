e8ab10fb0988042ca1b5f6c9f0bb279a62825496
schauhan2459 <130307427+schauhan2459@users.noreply.github.com>
2023-09-30 13:28:58 +0530

    Update server.js
    server

--- server.js
+++ server.js
@@ -73,3 +73,30 @@ app.get("/api/tasks", async (req, res) => {
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
+
+
+
+const io = require('socket.io')(3000, {
+    cors: {
+      origin: '*',
+    }
+  });
+const users = {};
+
+
+io.on('connection', socket => {
+    socket.on('new-user-joined', name => {
+        console.log("New user", name);
+        users[socket.id] = name;
+        socket.broadcast.emit('user-joined', name);
+    });
+
+    socket.on('send', message => {
+        socket.broadcast.emit('receive', { message: message, name: users[socket.id] })
+    });
+
+    socket.on('disconnect', users => {
+        socket.broadcast.emit('left', users[socket.id]);
+        delete users[socket.id];
+    });
+})
